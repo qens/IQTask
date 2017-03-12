ade3d71153a209bae7c5a9db280c63721a7e9309
Daniyar Bulgunayev <daniyar_bulgunayev@epam.com>
2017-03-13 00:01:21 +0600

    added target element and options(head) for choosing

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/**
+ * http://editor.method.ac/ - good site for creating or changing svg online
+ */
+
 class Cat {
     constructor(head, mustache, body, tail) {
         this.head = head;
@@ -6,7 +10,7 @@ class Cat {
         this.tail = tail;
     }
 
-    write(element) {
+    getElement() {
         let shape =`
         <svg width="200" height="250">
              <g id="left-ear">
@@ -27,7 +31,7 @@ class Cat {
             ${this.tail}
         </svg>
         `;
-        element.append(shape);
+        return shape;
     }
 }
 
@@ -35,6 +39,10 @@ const heads = {
     circle: '<ellipse id="head" stroke="#000" ry="30" rx="30" cy="67" cx="97" stroke-width="1.5" fill="#fff"/>',
     rect: '<rect id="head" height="55" width="55" y="41" x="68.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="#000" fill="#fff"/>',
     triangle: '<path id="head" stroke="#000" fill="#fff" stroke-width="1.5" d="m60.1239,98.7535l37.15346,-58.04335l37.15346,58.04335l-74.30691,0l-0.00001,0z" transform="rotate(180 97.27735900878906,69.73182678222656) "/>'
+};
+
+const mustaches = {
+
 };
 
 const bodies = {
@@ -45,14 +53,35 @@ const tails = {
     left: '<path stroke="#000" id="tail" d="m99.5,202c-16.14857,55 -41.22796,-35 -70.44082,19" opacity="0.5" stroke-opacity="null" stroke-width="2" fill="none"/>'
 };
 
+
+let targetCat = new Cat();
+let $target = $('#target');
+let $options = $('#options');
+
 $(document).ready(()=>{
     console.log('hello');
     let cat1 = new Cat(heads.circle, null, bodies.rect, tails.left);
     let cat2 = new Cat(heads.rect, null, bodies.rect, tails.left);
     let cat3 = new Cat(heads.triangle, null, bodies.rect, tails.left);
     let $body = $('body');
-    cat1.write($body);
-    cat2.write($body);
-    cat3.write($body);
+    $body.append(cat1.getElement());
+    $body.append(cat2.getElement());
+    $body.append(cat3.getElement());
+
+    fillOptions();
 });
 
+function fillOptions() {
+    for (let head in heads) {
+        if (heads.hasOwnProperty(head)) {
+            let $element = $(`<svg width="200" height="250">${heads[head]}</svg>`);
+            $element.click((e)=>{
+                targetCat.head = heads[head];
+                $target.html(targetCat.getElement());
+            });
+            $options.append($element);
+        }
+    }
+}
+
+// function writeOption()
